bf78780d7459613d2b1bf36401680b8e6f32a127
venkatesh <venkateshkasa996@gmail.com>
2023-11-02 09:09:14 +0530

    1st terra

---
+++ terra.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+ region = "us-east-1"
+ access_key = "AKIAXNJHMXFKIIWU35GI"
+ secret_key = "OMYLsNoW7VeAWDmQ8WsZV6aDXpW2hPeOmEnjwPSm"
+}
+resource "aws_instance" "example" {
+ ami = "ami-01bc990364452ab3e"
+ instance_type = "t2.micro"
+ count = 2
+tags = {
+        Name = "terratl"
+}
+}
+ 
